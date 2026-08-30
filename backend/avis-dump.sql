b67b22a7940185354883ecdacff62221b7ccf671
Your Name <you@example.com>
2026-08-30 03:00:03 +0200

    backup avis 2026-08-30-0300

--- backend/avis-dump.sql
+++ backend/avis-dump.sql
@@ -13,5 +13,5 @@ CREATE TABLE avis (
   );
 INSERT INTO avis VALUES(7,'omax.seduction',5,'J’ai eu l’occasion de travailler avec Ambre sur plusieurs projets vidéo et je suis très satisfait de son travail. Elle est réactive, professionnelle et comprend rapidement les attentes créatives. Au-delà de ses compétences techniques, Ambre sait apporter des idées pertinentes pour améliorer le rendu final et elle n’hésite pas à modifier les montages si besoin. C’est une monteuse fiable avec qui il est agréable de collaborer. Je la recommande sans hésitation.','https://www.tiktok.com/@omax.seduction','OMAX','/omax.jpeg','31200','2026-06-23 13:45:55');
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('avis',8);
+INSERT INTO sqlite_sequence VALUES('avis',11);
 COMMIT;
